c7ce7ef5d660aea351c24b5d984d078c74295347
docktermj <michael@dockter.com>
2023-10-18 09:02:46 -0400

    Split out examples

--- notifier/notifier_test.go
+++ notifier/notifier_test.go
@@ -54,23 +54,3 @@ func TestNotify(test *testing.T) {
 	}
 	Notify(ctx, observers, origin, subjectId, messageId, err, details)
 }
-
-// ----------------------------------------------------------------------------
-// Examples for godoc documentation
-// ----------------------------------------------------------------------------
-
-func ExampleNotify() {
-	// For more information, visit https://github.com/Senzing/go-observing/blob/main/notifier/notifier_test.go
-	ctx := context.TODO()
-	observers := &subject.SubjectImpl{}
-	origin := "Machine: 6 Process: Rover"
-	subjectId := 1
-	messageId := 2
-	var err error = nil
-	details := map[string]string{
-		"data": "aData",
-		"time": "aTime",
-	}
-	Notify(ctx, observers, origin, subjectId, messageId, err, details)
-	// Output:
-}
--- observer/observer_examples_test.go
+++ observer/observer_examples_test.go
@@ -1,71 +1,48 @@
+//go:build linux
+
 package observer
 
 import (
 	"context"
 	"fmt"
-	"os"
-	"testing"
-
-	"github.com/stretchr/testify/assert"
 )
 
 // ----------------------------------------------------------------------------
-// Test harness
+// Examples for godoc documentation
 // ----------------------------------------------------------------------------
 
-func TestMain(m *testing.M) {
-	err := setup()
-	if err != nil {
-		fmt.Print(err)
-		os.Exit(1)
-	}
-	code := m.Run()
-	err = teardown()
-	if err != nil {
-		fmt.Print(err)
-	}
-	os.Exit(code)
-}
-
-func setup() error {
-	var err error = nil
-	return err
-}
-
-func teardown() error {
-	var err error = nil
-	return err
-}
-
-// ----------------------------------------------------------------------------
-// Test interface functions
-// ----------------------------------------------------------------------------
-
-func TestObserverNull_GetObserverId(test *testing.T) {
+func ExampleObserverNull_GetObserverId() {
+	// For more information, visit https://github.com/Senzing/go-observing/blob/main/observer/observer_examples_test.go
 	ctx := context.TODO()
 	observer := &ObserverNull{
 		Id: "1",
 	}
-	assert.Equal(test, "1", observer.GetObserverId(ctx))
+	fmt.Print(observer.GetObserverId(ctx))
+	// Output: 1
 }
 
-func TestObserverNull_UpdateObserver(test *testing.T) {
+func ExampleObserverNull_UpdateObserver() {
+	// For more information, visit https://github.com/Senzing/go-observing/blob/main/observer/observer_test.go
 	ctx := context.TODO()
 	observer := &ObserverNull{
 		Id: "1",
 	}
 	observer.UpdateObserver(ctx, "A message")
+	// Output: Observer: 1;  Message: A message
 }
 
-func TestObserverWhiteList_GetObserverId(test *testing.T) {
+func ExampleObserverWhiteList_GetObserverId() {
+	// For more information, visit https://github.com/Senzing/go-observing/blob/main/observer/observer_test.go
 	ctx := context.TODO()
 	observer := &ObserverWhiteList{
 		Id: "1",
 	}
-	assert.Equal(test, "1", observer.GetObserverId(ctx))
+	fmt.Print(observer.GetObserverId(ctx))
+	// Output: 1
 }
 
-func TestObserverWhiteList_UpdateObserver(test *testing.T) {
+func ExampleObserverWhiteList_UpdateObserver() {
+	// For more information, visit https://github.com/Senzing/go-observing/blob/main/observer/observer_test.go
 	ctx := context.TODO()
 	message11 := `{"subjectId":"1", "messageId": "1"}`
 	message12 := `{"subjectId":"1", "messageId": "2"}`
@@ -90,4 +67,8 @@ func TestObserverWhiteList_UpdateObserver(test *testing.T) {
 	observer.UpdateObserver(ctx, message21)
 	observer.UpdateObserver(ctx, message22)
 	observer.UpdateObserver(ctx, message31)
+	// Output:
+	// Observer: 1;  Message: {"subjectId":"1", "messageId": "1"}
+	// Observer: 1;  Message: {"subjectId":"1", "messageId": "2"}
+	// Observer: 1;  Message: {"subjectId":"2", "messageId": "2"}
 }
